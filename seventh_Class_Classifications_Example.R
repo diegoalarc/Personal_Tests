8dcc2d3234ca617c65a6b3e8abeb2d559320131c
diegoalarc <diego.alarcondiaz@gmail.com>
2019-12-10 13:32:37 +0100

    New data use.

--- seventh_Class_Classifications_Example.R
+++ seventh_Class_Classifications_Example.R
@@ -3,11 +3,11 @@
 library(raster)
 library(cluster)
 
-raster_stack <- brick("C:/Users/JELG02/Documents/Clase_de_R/data/Nov_10m.grd")
+raster_stack <- brick("C:/Users/JELG02/Documents/Clase_de_R/data/Steigerwald/03_raster/01_landsat/01_indices/LC8_evi.grd")
 
 raster_df <- raster_stack[]
 
-kmeans_out <- kmeans(raster_df, 7, iter.max = 100, nstart = 10)
+kmeans_out <- kmeans(raster_df, 12, iter.max = 100, nstart = 10)
 
 kmeans_raster <- raster(raster_stack)
 kmeans_raster[] <- kmeans_out$cluster 
@@ -19,15 +19,24 @@ plot(kmeans_raster)
 library(RStoolbox)
 library(rgdal)
 
-td <- readOGR("C:/Users/JELG02/Documents/Clase_de_R/data/Training_data.shp")
-img <- brick("C:/Users/JELG02/Documents/Clase_de_R/data/Nov_10m.grd")
+td <- readOGR("C:/Users/JELG02/Documents/Clase_de_R/data/Landsat/RSE_book_Brazil_deforestation/p224r63_1988_Training_data.shp")
+img <- brick("C:/Users/JELG02/Documents/Clase_de_R/data/Landsat/RSE_book_Brazil_deforestation/p224r63_1988.grd")
 
 setwd("C:/Users/JELG02/Documents/Clase_de_R/data/")
 
 sc <- superClass(img, trainData = td,
-                 responseCol = "Class",
-                 model = "mlc",
+                 responseCol = "Class_name",
+                 model = "rf",
                  filename = "Classification.tif"
                  )
 
-plot(sc$map)
+plot(sc$map,
+     legend = FALSE,
+     col = c("red", "green", "black"), axes = TRUE,
+     main = "Classified Canopy LandCover Burn")
+
+legend("bottomleft",
+       legend = c("Deforestation", "Soil", "Trees"),
+       fill = c("red", "green", "black"),
+       border = FALSE,
+       bty = "y") # turn off legend border
