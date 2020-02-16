a22a4ce52ac39bd00276575ca5f6c31d4f9b5da6
diegoalarc <diego.alarcondiaz@gmail.com>
2020-02-16 15:23:02 +0100

    NDWI mean

--- Programming_and_Geostatistics_Test_v3.R
+++ Programming_and_Geostatistics_Test_v3.R
@@ -51,13 +51,6 @@ for (i in 1:length(roi_list)){
   names <- append(names,tmp3)
 }
 
-#################
-
-
-
-
-#################
-
 
 sequence <- seq(1,length(tmp)-9,10)
 brick_list <- list()
@@ -70,54 +63,124 @@ for (i in sequence){
   brick_list <- append(brick_list, tmp4)
 }
 
+
 ndwi <- list()
 n_list <- list()
 esp <- list()
 RGB_img <- list()
 t_img <- list()
-
+water_img <- list()
 
 for (i in 1:length(brick_list)){
 esp <- brick(brick_list[i])  
 ndwi <- ((esp[[6]]-esp[[8]])/(esp[[6]]+esp[[8]]))
 ndwi[ndwi>1] <- 1; ndwi[ndwi< (-1)] <- (-1)
-####RGB Brick####
-RGB_img <- brick((esp[[5]]),(esp[[6]]),(esp[[7]]))
+####Water classfy####
+water_img <- reclassify(ndwi, cbind(-Inf, 0.2, NA))
 ###########
 n_list <- append(n_list, ndwi)
 ####RGB List#####
-t_img <- append(t_img,RGB_img)
+t_img <- append(t_img,water_img)
 }
+
+
 
 tmp9 <- sort(names[sequence])
 names(n_list) <- names[sequence]
 n_list = n_list[order(names(n_list))]
+names(t_img) <- names[sequence]
+t_img = t_img[order(names(t_img))]
 
+###############################
+
+setwd("c:/Image_Landsat/test/")
+
+for (i in 1:length(t_img)){
+  #Sample image name
+  nm <- t_img[i]
+  #Extract year
+  yr <- substr(names(nm), start=18, stop=21)
+  #Extract month of year
+  mt <- substr(names(nm), start=22, stop=23)
+  #Extract day of year
+  dy <- substr(names(nm), start=24, stop=25) 
+  
+  img_fn <-  writeRaster(t_img[[i]],filename=paste0('Water','_',yr,'-',mt,'-',dy),format="GTiff",overwrite=TRUE)
+  rm(nm,yr,mt,dy)
+}
+
+tmp_Stack <- list()
+
+IMAGE_path2 <- "C:/Image_Landsat/NDWI/"
+##Load all the images Landsat8 in one list.
+all_IMAGE2 <- list.files(IMAGE_path2,
+                         full.names = TRUE,
+                         pattern = ".tif$")
+tmp_Stack <- stack(all_IMAGE2)
+
+# Define dataframe and fill it with the dates
+my_years <- c(names(tmp_Stack))
+my_mat <- matrix(data = NA, nrow = length(my_years), ncol = 4)
+my_mat[,1] <- my_years
+my_df <- data.frame(my_mat,stringsAsFactors=FALSE)
+names(my_df) <- c("Year", "maxValue", "minValue", "Mean_NDWI")
+
+# For-loop calculating mean of each raster and save it in data.frame
+for (i in 1:length(my_years)){
+  maxValue_layer <- c(maxValue(tmp_Stack[[i]]))
+  my_df[i,2] <- maxValue_layer
+  minValue_layer <- c(minValue(tmp_Stack[[i]]))
+  my_df[i,3] <- minValue_layer
+  mean_layer <- c((((maxValue(tmp_Stack[[i]]))+(minValue(tmp_Stack[[i]])))/2))
+  my_df[i,4] <- mean_layer
+  rm(maxValue_layer,minValue_layer,mean_layer,i)
+}
+
+###########################
+
+
+tmp10 <- list()
 
 setwd("C:/Image_Landsat/NDWI/")
 
 for (i in 1:length(n_list)){
-  writeRaster(n_list[[i]],filename = names(n_list[i]),format="GTiff",overwrite=TRUE)
-}
-
-#### Change Name ########
-
-setwd("C:/Image_Landsat/test/")
-
-for (i in 1:length(n_list)){
 #Sample image name
 nm <- n_list[i]
-
 #Extract year
 yr <- substr(names(nm), start=18, stop=21)
-
 #Extract month of year
 mt <- substr(names(nm), start=22, stop=23)
 #Extract day of year
 dy <- substr(names(nm), start=24, stop=25)
-
-writeRaster(n_list[[i]], filename=paste0(yr,'-',mt,'-',dy), format='GTiff', overwrite=T) 
+s_list <- writeRaster(n_list[[i]], filename=paste0('NDWI','_',yr,'-',mt,'-',dy), format='GTiff', overwrite=T) 
+tmp10 <- append(tmp10,s_list)
+rm(nm,yr,mt,dy)
 }
+
+
+#############
+
+IMAGE_path2 <- "C:/Image_Landsat/NDWI/"
+##Load all the images Landsat8 in one list.
+all_IMAGE2 <- list.files(IMAGE_path2,
+                        full.names = TRUE,
+                        pattern = ".tif$")
+
+tmp_Stack <- stack(all_IMAGE2)
+
+df_tmp_stack <- as.data.frame(tmp_Stack,xy=TRUE)
+
+plot(df_tmp_stack)
+
+
+
+
+head(tmp_Stack)
+
+
+
+
+
 ##### GLCM Analysis ####
 
 library(glcm)
@@ -132,11 +195,6 @@ plot(textureresult$glcm_variance)
 
 ###############RGB DATA#######
 
-setwd("B:/Image_Landsat/RGB/")
-
-for (i in 1:length(t_img)){
-img_rgb <-  writeRaster(t_img[[i]],filename = names(n_list[i]),format="GTiff",overwrite=TRUE)
-}
 ###############  Supervised classification ###############
 
 td <- readOGR("C:/Users/JELG02/OneDrive/Uni-Wue/1er_Semestre/MB2_Introduction_to_Programming_and_Geostatistics/Final_Project/Training_Area_utm.shp")
@@ -188,7 +246,20 @@ rasterEntropy(list_2[3])
 
 ##################Histogram
 library(ggplot2)
-n_stack <- stack(n_list)
+
+
+setwd("B:/")
+NDVI_HARV_path <- "Image_Landsat/NDWI/"
+
+ndwi_list <- list()
+ndwi_list <- list.files(NDVI_HARV_path,
+                        full.names = TRUE,
+                        pattern = ".tif$")
+
+
+n_stack <- stack(ndwi_list)
+n_stack[n_stack>1] <- 1; n_stack[n_stack< (-1)] <- (-1)
+
 
 ndwi_df <- as.data.frame(n_stack, xy = TRUE) %>%
   melt(id.vars = c('x','y'))
@@ -197,16 +268,3 @@ ggplot(ndwi_df) +
   geom_histogram(aes(value)) +
   facet_wrap(~variable)
 
-###################### Plot
-
-NDVI.stack <- stack(n_list)  # Representing your layerstack
-
-# Plot Stack, select point and automaticly extract values
-for (i in 1:length(n_list)){
-plot(NDVI.stack[[i]])  # One exemplary layer for orientation
-values <- click(NDVI.stack, n=1)
-}
-# Compose and plot dataframe
-timeseries <- data.frame(year = c(2000, 2005, 2010, 2015),
-                         values = values[1, ])
-plot(timeseries, type="l")
